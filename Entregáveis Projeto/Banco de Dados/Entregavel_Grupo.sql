03db0a3c8eb549abef4873db47d3cf8f73ec1413
ArthurMatos1 <arthur.matos@sptech.school>
2025-10-09 19:42:37 -0300

    Subindo Banco de dados e modelagoem nova

--- Entregáveis Projeto/Banco de Dados/Entregavel_Grupo.sql
+++ Entregáveis Projeto/Banco de Dados/Entregavel_Grupo.sql
@@ -1,70 +1,81 @@
-CREATE DATABASE AgroSense;
+CREATE DATABASE agrosense;
+use agrosense;
 
-USE AgroSense;
-
-CREATE TABLE cadastro(
+CREATE TABLE usuario(
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 nomeEmpresa VARCHAR(75),
 email VARCHAR(100) NOT NULL UNIQUE,
-constraint chkemail CHECK(email LIKE "%@%"),
+CONSTRAINT chkEmail CHECK(email LIKE '%@%'),
 senha VARCHAR(50) NOT NULL,
 telefone CHAR(11),
-cnpj char(11),
+cnpj CHAR(14),
 dtCadastro DATE,
-cep char(9),
-numEndereco int		
+cep CHAR(9),
+numEndereco INT
 );
 
-insert into cadastro(nomeEmpresa, email, senha, telefone, cnpj, dtCadastro, cep, numEndereco) values
+CREATE TABLE hectare(
+idHectare INT PRIMARY KEY AUTO_INCREMENT,
+nomeHectare VARCHAR(45) NOT NULL,
+kgPlantada INT NOT NULL,
+fkUsuario INT,
+CONSTRAINT fkUsuario FOREIGN KEY(fkUsuario) REFERENCES usuario(idUsuario)
+);
+
+
+CREATE TABLE subAreas(
+idSubArea INT,
+fkSubHectare INT,
+CONSTRAINT fkSubHect PRIMARY KEY (idSubArea, fkSubHectare),
+nomeArea VARCHAR(45),
+CONSTRAINT fkHectare FOREIGN KEY(fkSubHectare) REFERENCES hectare(idHectare)
+);
+
+CREATE TABLE sensores(
+idSensores INT PRIMARY KEY AUTO_INCREMENT,
+identi VARCHAR(40),
+dtInstalacao DATE,
+fkSubAreas INT, 
+CONSTRAINT fkSubArea FOREIGN KEY(fkSubAreas) REFERENCES subAreas(idSubArea)
+);
+
+CREATE TABLE medicao(
+idMedicao INT PRIMARY KEY AUTO_INCREMENT,
+umidade INT,
+fksensor INT,
+CONSTRAINT fkMedicaoSensor FOREIGN KEY(fksensor) REFERENCES sensores(idSensores)
+);
+
+-- INSERT TABELA USUARIO
+INSERT INTO usuario(nomeEmpresa, email, senha, telefone, cnpj, dtCadastro, cep, numEndereco) VALUES
 ('v8Tech', 'v8tech@email.teste', 'v812345', '11987654121', '12345678900001', '2025-02-12', '000000000', '45'),
 ('C6 Bank', 'c6bank@email.teste','c612345', '11987654321',  '43345678900004','2025-08-02', '000000001', '46'),
 ('Bradesco', 'bradesco@email.com', 'bra12345', '11987654321',  '12345678940007','2025-05-21', '000000002', '47'),
 ('ToTvs', 'ToTvs@email.com', 'totvs12345', '11987654321', '12345678904509' ,'2025-07-30', '000000003', '48');
 
+-- INSERT TABLE hectare
+INSERT INTO hectare (nomeHectare,KgPlantada,fkUsuario) VALUES
+('Hectare01','450',3),
+('Hectare02','600',2),
+('Hectare03','1500',4),
+('Hectare04','2340',1);
 
-CREATE TABLE hectare (
-idHectare INT PRIMARY KEY AUTO_INCREMENT,
-nomeHectare VARCHAR(45) NOT NULL,
-areaPlantada INT NOT NULL,
-qtdPlantada INT NOT NULL,
-fkSensores INT,
-constraint fkSensoresRegra
-foreign key (fkSensores)
-references sensores(idSensor),
-fkCadastro int,
-constraint fkCadastroRegra
-    foreign key (fkCadastro)
-        references cadastro(idUsuario)
-);
+-- INSERT TABELA subAreas
 
-insert into hectare (nomeHectare,areaPlantada,qtdPlantada,fkSensores)values
-('Hectare01','0001','450',3),
-('Hectare02','0002','600',2),
-('Hectare03','0003','1500',4),
-('Hectare04','0004','2340',1);
+INSERT INTO subAreas (idSubArea,fkSubHectare,nomeArea) VALUES
+(1,1, 'subArea 01'),
+(2,1, 'subArea 02'),
+(1,2, 'subArea 01'),
+(2,2, 'subArea 02');
+
+-- INSERT TABELA sensores
+-- modificar tabela sensores adicionando modelo de autoRelação
+insert into sensores(identi,dtInstalacao, fkSubAreas, idSubArea)values
+('3015','2025-08-10', 1),
+('2116','2025-07-18', 2),
+('2213','2025-09-05', 3),
+('2314','2025,10,28', 4);
 
 
-CREATE TABLE sensores (
-idSensor INT PRIMARY KEY AUTO_INCREMENT,
-identi VARCHAR(40),
-umidadeSolo int
-);
-insert into sensores(identi,umidadeSolo)values
-('3015','24'),
-('2116','18'),
-('2213','25'),
-('2314','32');
- 
- select cadastro.nomeEmpresa as 'Nome da Empresa', 
- cadastro.email as 'Email da Empresa',
- cadastro.senha as 'Senha da Empresa',
- cadastro.telefone as 'Telefone da Empresa',
- cadastro.dtCadastro as 'Data de Cadastro',
- hectare.nomeHectare as 'Identificação dos Hectares',
- hectare.areaPlantada as 'Área Plantada(ha)',
- hectare.qtdPlantada as 'Quantidade Plantada(Kg)',
- sensores.identi as 'Identificação dos Sensores',
- sensores.umidadeSolo as 'Umidade do Solo(%)'
- from cadastro join hectare on fkHectare = idHectare
- join sensores on fkSensores = idSensor;
- 
+
+
